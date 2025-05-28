b22118dff65da28fbb5a0864e0f8475f7189cfab
LeonGuertler <lg11@outlook.com>
2025-05-28 04:28:15 +0000

    wip

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 python3 unstable.py \
     --model_name "Qwen/Qwen3-4B-base" \
-    --train_env_id "SimpleTak-v0-train:2:qwen3-game" \
-    --eval_env_id "SimpleTak-v0-train:2:qwen3-game,Dataset-AIME24-v0-all:1:qwen3-reasoning "\
+    --train_env_id "SimpleTak-v0-train:2:qwen3-game,Nim-v0-train:2:qwen3-game,KuhnPoker-v0-train:2:qwen3-game,UltimateTicTacToe-v0-train:2:qwen3-game,SimpleNegotiation-v0-train:2:qwen3-game,LiarsDice-v0-train:2:qwen3-game,SpellingBee-v0-train:2:qwen3-game" \
+    --eval_env_id "SimpleTak-v0-train:2:qwen3-game,Nim-v0-train:2:qwen3-game,KuhnPoker-v0-train:2:qwen3-game,UltimateTicTacToe-v0-train:2:qwen3-game,SimpleNegotiation-v0-train:2:qwen3-game,LiarsDice-v0-train:2:qwen3-game,SpellingBee-v0-train:2:qwen3-game,Dataset-AIME24-v0:1:qwen3-reasoning"\
     --wandb \
     --num_actors 7 \
     --num_learners 1 \
@@ -13,13 +13,14 @@ python3 unstable.py \
     --bf16_training \
     --num_collection_workers 384 \
     --num_evaluation_workers 32 \
-    --lora_rank 64 \
-    --lora_alpha 64 \
+    --lora_rank 32 \
+    --lora_alpha 32 \
     --lora_dropout 0 \
-    --self_play_opponent_lag_lower 5 \
-    --self_play_opponent_lag_upper 9 \
+    --self_play_opponent_lag_lower 1 \
+    --self_play_opponent_lag_upper 7 \
     --format_reward_think 1.5 \
     --format_reward_valid_move 1.0 \
     --format_penalty_invalid_move -1.0\
     --eval_games_per_update_step 64\
-    --gradient_clip 0.05
+    --gradient_clip 0.5\
+    --lora_target_modules q_proj,k_proj,v_proj,o_proj,gate_proj,up_proj,down_proj
