d907ef740ce185313da49cf6b6f6200e8b644f39
tim-grams <t.grams339@gmail.com>
2025-06-24 00:38:01 +0200

    wip

---
+++ docs/conf.py
@@ -0,0 +1,25 @@
+import os
+import sys
+sys.path.insert(0, '..')
+
+# -- Project information -----------------------------------------------------
+project = "Unstable Baselines"
+copyright = "2025, Leon Guertler"
+author = "Leon Guertler, Tim Grams, Liu Zichen, Bobby Cheng"
+
+# -- General configuration ---------------------------------------------------
+master_doc = 'index'
+extensions = [
+    'sphinx.ext.napoleon',
+    'sphinx.ext.autodoc',
+    'sphinx.ext.viewcode'
+]
+templates_path = ["_templates"]
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+napoleon_google_docstring = True
+napoleon_numpy_docstring = False
+language = "en"
+
+# -- Options for HTML output -------------------------------------------------
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
