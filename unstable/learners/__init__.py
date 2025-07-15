0650985eeda6cff2f431db6e6604a0b6e8b16497
LeonGuertler <lg11@outlook.com>
2025-07-15 16:56:22 +0800

    minor fix

--- unstable/learners/__init__.py
+++ unstable/learners/__init__.py
@@ -1,10 +1,4 @@
-<<<<<<< HEAD
 from unstable.learners.reinforce_learner import REINFORCELearner
 from unstable.learners.a2c_learner import A2CLearner
 __all__ = ["REINFORCELearner", "A2CLearner"]
-=======
-from unstable.learners.standard_learner import StandardLearner
-from unstable.learners.a2c_learner import A2CLearner
-__all__ = ["StandardLearner", "A2CLearner"]
->>>>>>> main
 
